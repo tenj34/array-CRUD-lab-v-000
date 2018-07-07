9c2bfd03e941c9fa4ff3f3ba2f8669b20232fe90
Juan Tenezaca <tenj34@gmail.com>
2018-07-07 15:58:51 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,8 +15,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  phrase = array.pop
-  puts phrase
+  array.shift
 end
 
 def remove_element_from_start_of_array(array)
