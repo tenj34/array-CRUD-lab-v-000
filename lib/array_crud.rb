11e8bc0caf320a4cbfb0d979e0de426b46ec37f3
Juan Tenezaca <tenj34@gmail.com>
2018-07-07 15:50:29 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,13 @@ def create_an_empty_array
 end
 
 def create_an_array
-litty = ["1","2","3","4"]
+  litty = ["1","2","3","4"]
 end
 
 def add_element_to_end_of_array(array, element)
+  litty = ["1","2","3","4"]
 
+  litty << "5"
 end
 
 def add_element_to_start_of_array(array, element)
